536332c22a1d8634cc1c12e3bdc774ca2746ffc1
Kristian Matthews <kristian.matthews@me.com>
2016-03-01 23:34:35 +0000

    Update Gulp
    Added node modules and source paths, and added strict.

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,96 @@
-var gulp         = require('gulp'),
-    autoPrefixer = require('gulp-autoprefixer'),
-    browserify   = require('gulp-browserify'),
-    clean        = require('gulp-clean-dest'),
-    copy         = require('gulp-copy'),
-    combCSS      = require('gulp-csscomb'),
-    cssnano      = require('gulp-cssnano'),
-    merge        = require('gulp-merge'),
-    rename       = require('gulp-rename'),
-    sass         = require('gulp-sass');
+/*
+ * Copyright © 2014 - 2016 Kristian Matthews. All rights reserved.
+ */
+
+/*global require */
+var gulp         = require('gulp');
+var autoPrefixer = require('gulp-autoprefixer');
+var browserify   = require('gulp-browserify');
+var clean        = require('gulp-clean-dest');
+var copy         = require('gulp-copy');
+var cssComb      = require('gulp-csscomb');
+var cssNano      = require('gulp-cssnano');
+var merge        = require('gulp-merge');
+var rename       = require('gulp-rename');
+var sass         = require('gulp-sass');
 
 var config = {
-	contentPath: 'content/'
+    contentPath: 'content/',
+    nodeModulesPage: 'node_modules/',
+    srcPath: 'src/'
 };
 
 gulp.task('content:angularAnimate', function () {
-	return gulp.src(['**/*.js', '**/*.map'], {
-		cwd: 'node_modules/angular-animate/'
-	}).pipe(copy(config.contentPath + 'js/'));
+    'use strict';
+
+    return gulp.src(['**/*.js', '**/*.map'], {
+        cwd: config.nodeModulesPage + 'angular-animate/'
+    }).pipe(copy(config.contentPath + 'js/'));
 });
 
 gulp.task('content:angularMessages', function () {
-	return gulp.src(['**/*.js', '**/*.map'], {
-		cwd: 'node_modules/angular-messages/'
-	}).pipe(copy(config.contentPath + 'js/'));
+    'use strict';
+
+    return gulp.src(['**/*.js', '**/*.map'], {
+        cwd: config.nodeModulesPage + 'angular-messages/'
+    }).pipe(copy(config.contentPath + 'js/'));
 });
 
 gulp.task('content:angular', ['content:angularAnimate', 'content:angularMessages'], function () {
-	return gulp.src(['**/*.js', '**/*.map'], {
-		cwd: 'node_modules/angular/'
-	}).pipe(copy(config.contentPath + 'js/'));
+    'use strict';
+
+    return gulp.src(['**/*.js', '**/*.map'], {
+        cwd: config.nodeModulesPage + 'angular/'
+    }).pipe(copy(config.contentPath + 'js/'));
 });
 
 gulp.task('content:jquery', function () {
-	return gulp.src('**/**', {
-		cwd: 'node_modules/jquery/dist/'
-	}).pipe(copy(config.contentPath + 'js/'));
+    'use strict';
+
+    return gulp.src('**/**', {
+        cwd: config.nodeModulesPage + 'jquery/dist/'
+    }).pipe(copy(config.contentPath + 'js/'));
 });
 
 gulp.task('content:bootstrap', ['content:jquery'], function () {
-	return gulp.src('js/**', {
-		cwd: 'node_modules/bootstrap/dist/'
-	}).pipe(copy(config.contentPath));
+    'use strict';
+
+    return gulp.src('js/**', {
+        cwd: config.nodeModulesPage + 'bootstrap/dist/'
+    }).pipe(copy(config.contentPath));
 });
 
 gulp.task('content:trianglify', function () {
-	return gulp.src('**/**', {
-		cwd: 'node_modules/trianglify/dist/'
-	}).pipe(copy(config.contentPath + 'js/'));
+    'use strict';
+
+    return gulp.src('**/**', {
+        cwd: config.nodeModulesPage + 'trianglify/dist/'
+    }).pipe(copy(config.contentPath + 'js/'));
 });
 
 gulp.task('content', ['content:angular', 'content:bootstrap', 'content:jquery', 'content:trianglify']);
 
 gulp.task('scripts', ['content'], function () {
-	return gulp.src('src/js/polar.js')
-		.pipe(browserify())
-		.pipe(gulp.dest(config.contentPath + 'js/'));
+    'use strict';
+
+    return gulp.src(config.srcPath + 'js/polar.js')
+        .pipe(browserify())
+        .pipe(gulp.dest(config.contentPath + 'js/'));
 });
 
 gulp.task('styles', function () {
-	return gulp.src('src/scss/polar.scss')
-		.pipe(combCSS())
-		.pipe(sass())
-		.pipe(autoPrefixer())
-		.pipe(gulp.dest(config.contentPath + 'css/'))
-		.pipe(cssnano())
-		.pipe(rename({
-			extname: '.min.css'
-		}))
-		.pipe(gulp.dest(config.contentPath + 'css/'));
+    'use strict';
+
+    return gulp.src(config.srcPath + 'scss/polar.scss')
+        .pipe(cssComb())
+        .pipe(sass())
+        .pipe(autoPrefixer())
+        .pipe(gulp.dest(config.contentPath + 'css/'))
+        .pipe(cssNano())
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest(config.contentPath + 'css/'));
 });
 
 gulp.task('build', ['scripts', 'styles']);
