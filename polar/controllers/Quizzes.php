f75d2534c7e0cb51c176df32e9b05c3583bfe53d
Kristian Matthews <kristian.matthews@me.com>
2016-03-23 23:09:08 +0000

    Created Quizzes Controller

---
+++ polar/controllers/Quizzes.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @copyright Copyright © 2014 - 2016 Kristian Matthews. All rights reserved.
+ * @author    Kristian Matthews <kristian.matthews@my.westminster.ac.uk>
+ * @package   Polar
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Quizzes controller.
+ */
+class Quizzes extends POLAR_Controller {
+
+	/**
+	 * Quizzes constructor.
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->auth->authed();
+	}
+
+	/**
+	 * Index.
+	 */
+	public function index()
+	{
+		$this->template->view('default', 'welcome_message');
+	}
+
+	/**
+	 * Quiz slug.
+	 *
+	 * @param string $slug Slug.
+	 */
+	public function quiz()
+	{
+		$this->template->view('default', 'quiz');
+	}
+}
