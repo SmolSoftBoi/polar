7924903a412524200b1aba2fc1c5433b8171c3e3
Kristian Matthews <kristian.matthews@me.com>
2016-04-04 23:55:49 +0100

    Updated Connection Model
    Loaded quiz and user models, updated `set_item()` to update instead of insert possible existing connection and add quiz connection, updated `build()`, `generate()`, and `clean()`.

--- polar/models/Connection_model.php
+++ polar/models/Connection_model.php
@@ -11,12 +11,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * Connection model.
  *
  * @package Polar\Models
+ *
+ * @property Quiz_model $quiz_model Quiz model.
+ * @property User_model $user_model User model.
  */
 class Connection_model extends Item_model {
 
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model(array('quiz_model', 'user_model'));
 
 		$this->clean();
 	}
@@ -54,7 +58,33 @@ class Connection_model extends Item_model {
 	 */
 	public function set_item($connection_item)
 	{
-		return $this->base_set_item('connections', 'connection_id', 'connection_id', 'connection_item', $connection_item);
+		if ( ! isset($connection_item->connection_id) || $connection_item->connection_id === 0)
+		{
+			$connection_params = new Connection_params();
+
+			$connection_params->user_id = $connection_item->user_id;
+
+			$connection_items = $this->search($connection_params);
+
+			if (count($connection_items) > 0)
+			{
+				$connection_item->connection_id = reset($connection_items)->connection_id;
+			}
+		}
+
+		$connection_id = $this->base_set_item('connections', 'connection_id', 'connection_id', $connection_item);
+
+		if (isset($connection_item->quiz))
+		{
+			$quiz_connection = array(
+				'quiz_id' => $connection_item->quiz->quiz_id,
+				'connection_id' => $connection_id
+			);
+
+			$this->db->insert('quiz_connections', $quiz_connection);
+		}
+
+		return $connection_id;
 	}
 
 	/**
@@ -71,9 +101,10 @@ class Connection_model extends Item_model {
 
 		$this->base_build('connections');
 
-		$this->db->join('quiz_connections', 'connections.connection_id = quiz_connections.connection_id');
+		$this->db->join('quiz_connections', 'connections.connection_id = quiz_connections.connection_id', 'left');
 
 		$this->build_param($connection_params, 'quiz_id', 'quiz_connections', 'quiz_id');
+		$this->build_param($connection_params, 'user_id', 'connections', 'user_id');
 	}
 
 	/**
@@ -88,7 +119,32 @@ class Connection_model extends Item_model {
 		$connection_item = $this->base_generate(2, 'connection_id', $connection_item);
 
 		$connection_item->user_id = intval($connection_item->user_id);
-		$connection_item->connection_timestamp = new DateTime($connection_item->connection_timestamp);
+
+		$timestamp = new DateTime();
+
+		$timestamp->setTimestamp($connection_item->connection_timestamp);
+
+		$connection_item->connection_timestamp = $timestamp;
+
+		if ($this->level > 0)
+		{
+			$this->user_model->level = $this->level;
+
+			$connection_item->user = $this->user_model->get_item($connection_item->user_id);
+
+			$quiz_params = new Quiz_params();
+
+			$quiz_params->connection_id = $connection_item->connection_id;
+
+			$this->quiz_model->level = $this->level;
+
+			$quiz_connections = $this->quiz_model->search($quiz_params);
+
+			if (count($quiz_connections) > 0)
+			{
+				$connection_item->quiz = reset($quiz_connections);
+			}
+		}
 
 		return $connection_item;
 	}
@@ -100,8 +156,8 @@ class Connection_model extends Item_model {
 	{
 		$timestamp = new DateTime();
 
-		$timestamp->sub(new DateInterval('T30S'));
+		$timestamp->sub(new DateInterval('PT30S'));
 
-		$this->db->where('connection_timestamp', '< ' . $timestamp->getTimestamp())->delete('connections');
+		$this->db->where('connection_timestamp <', $timestamp->getTimestamp())->delete('connections');
 	}
 }
